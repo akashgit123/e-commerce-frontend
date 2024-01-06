7608d0f20267b149fa39a77748781323d46bd075
Akash <akashshettigar709@gmail.com>
2024-01-06 16:04:10 +0530

    helmet package for meta tags

--- src/components/layouts/Layout.js
+++ src/components/layouts/Layout.js
@@ -1,15 +1,29 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import Header from "./Header";
 import Footer from "./Footer";
 
-function Layout(props) {
+function Layout({ children, title, description, keywords, author }) {
   return (
     <div>
+      <Helmet>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta name="author" content={author} />
+      </Helmet>
       <Header />
-      <main style={{ height: "80vh" }}>{props.children}</main>
+      <main style={{ height: "80vh" }}>{children}</main>
       <Footer />
     </div>
   );
 }
+
+Layout.defaultProps = {
+  title: "UrShop",
+  description: "MERN Stack Project",
+  keywords: "Mongodb , Express , Nodejs , React",
+  author: "Akash",
+};
 
 export default Layout;
