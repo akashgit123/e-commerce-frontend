5392eee731542429e9ab7d76d88aceda40f6294f
Akash <akashshettigar709@gmail.com>
2024-01-06 12:02:01 +0530

    About Contact Page not found

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Layout from "../components/layouts/Layout";
+import { Link } from "react-router-dom";
 
 function PageNotFound() {
   return (
     <Layout>
-      <div>PageNotFound</div>
+      <div className="pnf">
+        <h1 className="pnf-title">404</h1>
+        <h2 className="pnf-heading">Oops ! Page Not Found</h2>
+        <Link to="/" className="pnf-btn">
+          Go Back
+        </Link>
+      </div>
     </Layout>
   );
 }
