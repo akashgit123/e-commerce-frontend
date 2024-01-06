8d87abf2cb71395342ba245405715c7c5e1e23e7
Akash <akashshettigar709@gmail.com>
2024-01-06 11:27:45 +0530

    Header and footer

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <div className="bg-dark text-white p-2 position-absolute bottom-0 w-100">
+    <div className="footer bg-dark text-white p-2 position-absolute bottom-0 w-100">
       <h6 className="text-center">All Rights Reserved &copy; UrShop</h6>
+      <p className="text-center mt-1 mb-0">
+        <Link to="/about"> About </Link> |
+        <Link to="/policy"> Privacy Policy </Link> |
+        <Link to="/contact"> Contact</Link> |
+      </p>
     </div>
   );
 }
--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,7 +1,60 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <NavLink className="navbar-brand" to="/">
+              🛍️UrShop
+            </NavLink>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink className="nav-link active" aria-current="page" to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/categories">
+                  Categories
+                </NavLink>
+              </li>
+            </ul>
+            <div className="d-flex navbar-nav mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/cart">
+                  🛒 Cart
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/signup">
+                  Signup
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/login">
+                  Login
+                </NavLink>
+              </li>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Header;
