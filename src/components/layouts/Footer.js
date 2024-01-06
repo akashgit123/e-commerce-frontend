b6e7e86b0c41410a9b46e1f7b29f26042a29c656
Akash <akashshettigar709@gmail.com>
2024-01-06 10:40:03 +0530

    first commit and react router

---
+++ src/components/layouts/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="bg-dark text-white p-2 position-absolute bottom-0 w-100">
+      <h6 className="text-center">All Rights Reserved &copy; UrShop</h6>
+    </div>
+  );
+}
+
+export default Footer;
